02e72786da594f8c40585a6d44d94737a8dfce1a
ShilohAlleyne <ShilohAlleyne@gmail.com>
2024-12-11 12:25:17 +0000

    New snacks dashboard, wsl copy paste

--- init.lua
+++ init.lua
@@ -1,5 +1,20 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- require("lspconfig").gleam.setup({})
 require("nvim-treesitter.install").compilers = { "zig" }
+
+-- Clipboard
+vim.opt.clipboard:append("unnamedplus")
+
+vim.g.clipboard = {
+    name = 'wsl-clip',
+    copy = {
+        ['+'] = 'clip.exe',
+        ['*'] = 'clip.exe',
+    },
+    paste = {
+        ['+'] = 'powershell.exe Get-Clipboard',
+        ['*'] = 'powershell.exe Get-Clipboard',
+    },
+    cache_enabled = 0,
+}
